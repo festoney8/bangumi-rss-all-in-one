3bac6cb477af79d531e0dfdd7fabb477f056764b
festoney8 <festoney8@gmail.com>
2023-04-21 15:29:08 +0800

    add logger

---
+++ utils/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+dir_name = os.path.dirname(__file__)
+
+logging.basicConfig(
+    # level=logging.INFO,
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s: %(message)s',
+    datefmt='%Y/%m/%d %H:%M:%S',
+    # filename=os.path.join(dir_name, "bangumi_rss.log"),
+    # filemode='a+',
+    handlers=[
+        logging.FileHandler(os.path.join(dir_name, "bangumi_rss.log"), mode='a+'),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger()
